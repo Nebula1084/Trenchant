248d66a1448b346262feef9c01e64c683278f80b
Sea <hjw_redeption@outlook.com>
2015-12-28 23:46:49 -1200

    ward model

--- static/js/shader/Ward.js
+++ static/js/shader/Ward.js
@@ -1,5 +1,5 @@
 /* global Trenchant */
-Trenchant.Torrance_Sparrow = function(gl){
+Trenchant.Ward = function(gl){
     var str = "\
 			precision mediump float;\
 		\
@@ -33,8 +33,8 @@ Trenchant.Torrance_Sparrow = function(gl){
 				if (!uUseLighting) {\
 					lightWeighting = vec3(1.0, 1.0, 1.0);\
 				} else {\
-                    vec3 L = normalize(uPointLightingLocation - vPosition.xyz);\
-                    vec3 N = normalize(vTransformedNormal);\
+                    vec3 L = normalize(-uPointLightingLocation + vPosition.xyz);\
+                    vec3 N = normalize(-vTransformedNormal);\
                     vec3 E = normalize(vPosition.xyz);\
                     vec3 H = normalize(L + E);\
         \
@@ -43,17 +43,15 @@ Trenchant.Torrance_Sparrow = function(gl){
                     float EH = dot(E, H);\
                     float NE = dot(N, E);\
                     float G = min(1.0, 2.0*NH*NE/EH);\
+                    float omega = 0.1;\
+                    float gamma = acos(NH);\
                     G = min(G, 2.0*NH*NL/EH);\
                     \
-                    float c = EH;\
-                    float g = sqrt(Kn*Kn + c*c -1.0);\
-                    float f1=g-c/g+c;\
-                    float f2=(c*(g+c)-1.0)/(c*(g-c)+1.0);\
-                    float F = Kr+(1.0-Kr)*pow((1.0-NL), 5.0);\
                     float alpha=acos(NH);\
                     float D = Kc*exp(-pow(alpha/Km, 2.0));\
 					float diffuseLightWeighting = max(dot(N, L), 0.0);\
-                    float specularLightWeighting = F*D*G/NE;\
+                    float ward=exp(-pow(tan(gamma)/omega, 2.0))*sqrt(NL/NE)/4.0/pow(gamma,2.0);\
+                    float specularLightWeighting = ward;\
 					lightWeighting = uAmbientColor\
                         + uPointLightingSpecularColor * specularLightWeighting\
 						+ uPointLightingDiffuseColor * diffuseLightWeighting;\
